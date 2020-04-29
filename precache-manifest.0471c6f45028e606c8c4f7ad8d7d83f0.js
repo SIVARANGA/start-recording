7867030d28a17a22bb4ff6052a23fd5cb6c00905
Raja <gkrajareddy@gmail.com>
2020-04-29 17:06:55 +0530

    Updates

---
+++ precache-manifest.0471c6f45028e606c8c4f7ad8d7d83f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b03ef1f0fbf9d4b9a771d96b27af108",
+    "url": "/start-recording/index.html"
+  },
+  {
+    "revision": "bedb12f1bc6f30289b96",
+    "url": "/start-recording/static/css/main.6826cc26.chunk.css"
+  },
+  {
+    "revision": "4b61fbe0ceb6a87be85e",
+    "url": "/start-recording/static/js/2.65b4025e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/start-recording/static/js/2.65b4025e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedb12f1bc6f30289b96",
+    "url": "/start-recording/static/js/main.174bc0ce.chunk.js"
+  },
+  {
+    "revision": "2e4dc212cc48f6465cae",
+    "url": "/start-recording/static/js/runtime-main.8121e593.js"
+  }
+]);
